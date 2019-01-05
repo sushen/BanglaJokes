0bbeff29ce84bb9799afcfc67c14a06cc43db98a
sushen <sushenbiswasaga@gmail.com>
2019-01-05 10:24:37 +0600

    Initial commit

---
+++ app/src/main/java/com/meshstocks/jokes/banglajokes/MainActivity.java
@@ -0,0 +1,28 @@
+package com.meshstocks.jokes.banglajokes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView mTitleWindow = (TextView) findViewById(R.id.titleWindow);
+        TextView mMessageWindow = (TextView) findViewById(R.id.messageWindow);
+
+        mTitleWindow.setText("নিজেই দাঁড়ানোর জায়গা পায় না আবার আরেকটারেও দাঁড় করাইয়া দিছে।");
+
+//        StringBuilder stringBuilder = new StringBuilder();
+////
+////        String someMassage = " Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.\n" +
+////                "\n" +
+////                "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham. ";
+////
+////        mMessageWindow.setText(stringBuilder.toString());
+        mMessageWindow.setText(" বাসে প্রচণ্ড ভিড় । এক মেয়ের পিছনে এক ছোকড়া দাঁড়িয়েছে। কিছুক্ষণ পর পিছন ফিরে মেয়ে ঠাস করে চড় মেরে বসল ছোকড়াকে! ফাজিল ! বারবার ধাক্কা খাইতাছে , নিজেই দাঁড়ানোর জায়গা পায় না আবার আরেকটারেও দাঁড় করাইয়া দিছে। ");
+
+    }
+}
